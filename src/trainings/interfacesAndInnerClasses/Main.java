28264236aae6e35cd6f0c34adf7297dafe6c8d01
DavidShahbazyan <DavidShahbazyan@users.noreply.github.com>
2014-11-11 11:14:20 +0400

    Preparing to homework.

---
+++ src/trainings/interfacesAndInnerClasses/Main.java
@@ -0,0 +1,8 @@
+package trainings.interfacesAndInnerClasses;
+
+/**
+ * @author David.Shahbazyan
+ * @since Nov 11, 2014.
+ */
+public class Main {
+}
---
+++ src/trainings/interfacesAndInnerClasses/Shape.java
@@ -0,0 +1,17 @@
+package trainings.interfacesAndInnerClasses;
+
+/**
+ * @author David.Shahbazyan
+ * @since Nov 11, 2014.
+ */
+public interface Shape {
+    /**
+     * @return The shape's type
+     */
+    String getShapeType();
+
+    /**
+     * @return The shape's name
+     */
+    String getShapeName();
+}
