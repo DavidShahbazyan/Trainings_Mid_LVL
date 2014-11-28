d723137fee273b60dff7151df9b2b33d80ecdf40
DavidShahbazyan <DavidShahbazyan@users.noreply.github.com>
2014-11-28 16:55:06 +0400

    Exceptions template [new, better version than the last version :) ]...

--- src/com/synisys/trainings/mid/exceptionsAndAssertions/Main.java
+++ src/com/synisys/trainings/mid/exceptionsAndAssertions/Main.java
@@ -7,22 +7,26 @@ package com.synisys.trainings.mid.exceptionsAndAssertions;
 public class Main {
     public static void main(String[] args) {
         try {
-//            test(1);
-            test(2);
-//            test(-2);
+            test(321564136, 2);
+//            test(10, 10);
+//            test(321564136, 999999999);
+//            test(1000, 0);
         } catch (UnsupportedArgumentException e) {
             e.printStackTrace();
         }
     }
 
-    private static void test(int i) throws UnsupportedArgumentException {
-        if (i != 2) {
-            throw new UnsupportedArgumentException("The method's argument should be equal to 2.");
+    private static void test(Integer n1, Integer n2) throws UnsupportedArgumentException {
+        if (n1.compareTo(n2) == -1) {
+            throw new UnsupportedArgumentException("Attention [argument1] must be grader or equal to [argument2]!");
         }
-        int counter = 0;
-        for (int j = i; j < 999999999; j *= i) {
-            System.out.println("POW(2,"+ ++counter + ") = " + j);
+        try {
+            while (n1 % n2 == 0) {
+                System.out.println(n1 /= n2);
+            }
+            System.out.println("The minimum value is reached!");
+        } catch (ArithmeticException ex) {
+            throw new UnsupportedArgumentException("The method's arguments are not correct!", ex);
         }
-        System.out.println("The maximum number for int is reached!");
     }
 }
