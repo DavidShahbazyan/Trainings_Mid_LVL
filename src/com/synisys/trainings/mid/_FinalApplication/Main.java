172aad5ae6657a269b1816aeb84969f69b05f90a
DavidShahbazyan <DavidShahbazyan@users.noreply.github.com>
2014-12-17 23:43:41 +0400

    _FinalApplication package added.

---
+++ src/com/synisys/trainings/mid/_FinalApplication/Main.java
@@ -0,0 +1,11 @@
+package com.synisys.trainings.mid._FinalApplication;
+
+/**
+ * @author David Shahbazyan
+ * @since 17.12.2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I'm the final application.");
+    }
+}
