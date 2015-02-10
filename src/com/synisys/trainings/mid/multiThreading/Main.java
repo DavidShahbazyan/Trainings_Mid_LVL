6f19ad2fb8234499b82413b5098eaa559c4ffea8
David Shahbazyan <DavidShahbazyan@users.noreply.github.com>
2015-02-10 08:49:29 +0400

    Uncaught Exception Handler Implementation.

--- src/com/synisys/trainings/mid/multiThreading/Main.java
+++ src/com/synisys/trainings/mid/multiThreading/Main.java
@@ -1,11 +1,35 @@
 package com.synisys.trainings.mid.multiThreading;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author David.Shahbazyan
  * @since Nov 11, 2014.
  */
 public class Main {
     public static void main(String[] args) {
-
+        final AtomicInteger atomicInteger = new AtomicInteger(0);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                int threadId = atomicInteger.incrementAndGet();
+                for (int i = 5; i <= 0; i--) {
+                    System.out.println("Thread No. " + threadId + ": " + 5 / i);
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
     }
 }
